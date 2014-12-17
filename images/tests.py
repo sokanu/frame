a3865a3bdff2a3687adea0a45c7fda78ffe46115
Jean-Marc Skopek <jskopek@gmail.com>
2014-12-17 14:25:42 -0500

    Added basic view

--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,10 @@
+from django.test import Client
 from django.test import TestCase
 
 # Create your tests here.
+
+class ImageViewTestCase(TestCase):
+    def test_request(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.content, 'bok')
