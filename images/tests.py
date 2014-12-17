8e1c7bc0394c88bc890fceeceeedefb79687ca44
Jean-Marc Skopek <jskopek@gmail.com>
2014-12-17 14:41:36 -0500

    Requests stupidly seve a static image

--- images/tests.py
+++ images/tests.py
@@ -1,10 +1,14 @@
 from django.test import Client
 from django.test import TestCase
+import os
 
-# Create your tests here.
 
 class ImageViewTestCase(TestCase):
     def test_request(self):
         c = Client()
         response = c.get('/')
-        self.assertEqual(response.content, 'bok')
+
+        image_path = os.path.dirname(os.path.realpath(__file__)) + '/test_resources/frame.jpg'
+        expected_data = open(image_path, 'r').read()
+
+        self.assertEqual(response.content, expected_data)
--- images/views.py
+++ images/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponse
+import os
 
 # Create your views here.
 
 class ImageView(View):
     def get(self, request):
-        return HttpResponse('ok')
+        image_path = os.path.dirname(os.path.realpath(__file__)) + '/test_resources/frame.jpg'
+        img = open(image_path, 'r')
+        data = img.read()
+
+        response = HttpResponse(data, content_type='image/jpg')
+        return response
 
