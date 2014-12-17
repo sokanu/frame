4eca71dc265b17f0a6cf2aca6adc1fc521950a02
Jean-Marc Skopek <jskopek@gmail.com>
2014-12-17 17:26:58 -0500

    Added ability to modify width and height of requested image

--- images/tests.py
+++ images/tests.py
@@ -1,14 +1,86 @@
 from django.test import Client
 from django.test import TestCase
 import os
+from PIL import Image
+import StringIO
+
+# helper
+def initialize_request_image(url):
+    c = Client()
+    response = c.get(url)
+    response_data = StringIO.StringIO(response.content)
+    response_image = Image.open(response_data)
+    return response_image
 
 
+# tests
 class ImageViewTestCase(TestCase):
-    def test_request(self):
-        c = Client()
-        response = c.get('/')
+    def test_response_image(self):
+        image = initialize_request_image('/')
 
         image_path = os.path.dirname(os.path.realpath(__file__)) + '/test_resources/frame.jpg'
-        expected_data = open(image_path, 'r').read()
+        expected_image = Image.open(image_path)
 
-        self.assertEqual(response.content, expected_data)
+        self.assertEqual(image.size, expected_image.size)
+
+    def test_response_image_resize_width(self):
+        image = initialize_request_image('/')
+
+        image_path = os.path.dirname(os.path.realpath(__file__)) + '/test_resources/frame.jpg'
+        expected_image = Image.open(image_path)
+
+        # test initial response size
+        self.assertEqual(image.size[0], expected_image.size[0])
+        self.assertNotEqual(image.size[0], 100)
+
+        # request modified width image
+        modified_width_image = initialize_request_image('/?width=100')
+
+        # test modified response size
+        self.assertEqual(modified_width_image.size[0], 100)
+        self.assertNotEqual(image.size[0], modified_width_image.size[0])
+
+        # ensure ratios are identical
+        self.assertNotEqual(image.size[0] / float(image.size[1]), modified_width_image.size[0] / float(modified_width_image.size[1])) 
+        
+    def test_response_image_resize_height(self):
+        image = initialize_request_image('/')
+
+        image_path = os.path.dirname(os.path.realpath(__file__)) + '/test_resources/frame.jpg'
+        expected_image = Image.open(image_path)
+
+        # test initial response size
+        self.assertEqual(image.size[1], expected_image.size[1])
+        self.assertNotEqual(image.size[1], 100)
+
+        # request modified width image
+        modified_height_image = initialize_request_image('/?height=100')
+
+        # test modified response size
+        self.assertEqual(modified_height_image.size[1], 100)
+        self.assertNotEqual(image.size[1], modified_height_image.size[1])
+
+        # ensure ratios are identical
+        self.assertNotEqual(image.size[0] / float(image.size[1]), modified_height_image.size[0] / float(modified_height_image.size[1])) 
+ 
+    def test_response_image_resize_width_height(self):
+        # request modified height & width image
+        modified_image = initialize_request_image('/?width=100&height=100')
+
+        # test modified response size
+        self.assertEqual(modified_image.size[0], 100)
+        self.assertEqual(modified_image.size[1], 100)
+
+    def test_response_larger_image(self):
+        image = initialize_request_image('/')
+        larger_image = initialize_request_image('/?width=%d' % (image.size[0] * 2))
+
+        self.assertEqual(image.size[0] * 2, larger_image.size[0])
+        self.assertEqual(image.size[1] * 2, larger_image.size[1])
+
+    def test_image_sizes_are_integers(self):
+        image = initialize_request_image('/?width=100.432')
+        self.assertEqual(image.size[0], 100)
+
+        image = initialize_request_image('/?height=150.432')
+        self.assertEqual(image.size[1], 150)
--- images/views.py
+++ images/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponse
+from PIL import Image
+import StringIO
 import os
 
 # Create your views here.
@@ -8,9 +10,28 @@ import os
 class ImageView(View):
     def get(self, request):
         image_path = os.path.dirname(os.path.realpath(__file__)) + '/test_resources/frame.jpg'
-        img = open(image_path, 'r')
-        data = img.read()
+        image_data = open(image_path, 'r')
 
-        response = HttpResponse(data, content_type='image/jpg')
+        image = Image.open(image_data)
+
+        if request.GET.get('width') or request.GET.get('height'):
+            image_ratio = image.size[0] / float(image.size[1]) #  width / height
+
+            if request.GET.get('width') and request.GET.get('height'):
+                width = int(float(request.GET['width']))
+                height = int(float(request.GET['height']))
+            elif request.GET.get('width'):
+                width = int(float(request.GET['width']))
+                height = int(width / image_ratio)
+            elif request.GET.get('height'):
+                height = int(float(request.GET['height']))
+                width = int(height * image_ratio)
+            else:
+                raise Exception('Internal Error. Something really strange happened')
+
+            image = image.resize((width, height))
+
+        response = HttpResponse(content_type='image/jpeg')
+        image.save(response, 'jpeg')
         return response
 
