884b39053b4eddfb22073c13ab5b2b2c75bdb60e
Jean-Marc Skopek <jskopek@gmail.com>
2014-12-17 19:57:16 -0500

    Moved image modifiers to a separate file

--- images/views.py
+++ images/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponse
+from images.modifiers import SizeModifier
+from images.modifiers import QualityModifier
 from PIL import Image
 import StringIO
 import os
@@ -11,33 +13,12 @@ class ImageView(View):
     def get(self, request):
         image_path = os.path.dirname(os.path.realpath(__file__)) + '/test_resources/frame.jpg'
         image_data = open(image_path, 'r')
-
         image = Image.open(image_data)
 
-        if request.GET.get('width') or request.GET.get('height'):
-            image_ratio = image.size[0] / float(image.size[1]) #  width / height
-
-            if request.GET.get('width') and request.GET.get('height'):
-                width = int(float(request.GET['width']))
-                height = int(float(request.GET['height']))
-            elif request.GET.get('width'):
-                width = int(float(request.GET['width']))
-                height = int(width / image_ratio)
-            elif request.GET.get('height'):
-                height = int(float(request.GET['height']))
-                width = int(height * image_ratio)
-            else:
-                raise Exception('Internal Error. Something really strange happened')
-
-            image = image.resize((width, height))
-
-        if request.GET.get('quality') and not float(request.GET['quality']) >= 100:
-            quality = int(float(request.GET['quality']))
-            quality_modified_data = StringIO.StringIO()
-            image.save(quality_modified_data, 'jpeg', quality=quality)
-            quality_modified_data.seek(0)
-            image = Image.open(quality_modified_data)
-
+        # apply modifiers
+        modifiers = [SizeModifier, QualityModifier]
+        for modifier_class in modifiers:
+            image = modifier_class(image=image, params=request.GET).run().image
 
         response = HttpResponse(content_type='image/jpeg')
         image.save(response, 'jpeg')
