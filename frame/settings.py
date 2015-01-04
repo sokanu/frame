cfb2ef5dc079a1376b8ec39de6145766e5bdd8dc
Jean-Marc Skopek <jskopek@gmail.com>
2015-01-04 02:29:06 -0800

    Added more flexibility to database configurations

--- frame/settings.py
+++ frame/settings.py
@@ -62,16 +62,15 @@ WSGI_APPLICATION = 'frame.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+import dj_database_url
 
-DATABASES = {
-    'default': {
+if dj_database_url.config():
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+    }}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
