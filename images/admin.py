28d647b8d104630fd1dcc6f88b474ee0869d2a95
Jean-Marc Skopek <jskopek@gmail.com>
2015-01-04 02:08:05 -0800

    Enhanced image admin

--- images/admin.py
+++ images/admin.py
@@ -3,5 +3,10 @@ from images.models import Image
 from images.models import S3Connection
 
 # Register your models here.
-admin.site.register(Image)
-admin.site.register(S3Connection)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('hash', 'variation')
+
+@admin.register(S3Connection)
+class S3ConnectionAdmin(admin.ModelAdmin):
+    list_display = ('bucket',)
