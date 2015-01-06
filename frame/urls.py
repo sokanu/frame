aaac333e20843369e834749eb9c88f6eed807cd2
Jean-Marc Skopek <jskopek@gmail.com>
2015-01-06 00:41:02 -0800

    Fixed csrf exemption

--- frame/urls.py
+++ frame/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from django.conf import settings
 from images.views import ImageUploaderView
 from images.views import ImageView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^upload/$', ImageUploaderView.as_view()),
+    url(r'^upload/$', csrf_exempt(ImageUploaderView.as_view())),
     url(r'^(?P<image_identifier>[^/]+)/$', ImageView.as_view(), name='image'),
 ) 
 
